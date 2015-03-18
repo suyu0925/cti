7176aee9a8c91f504055988082b9b8244110a4c3
kivvi.yusu <kivvi.yusu@gmail.com>
2015-03-19 03:26:00 +0800

    temp commit.

---
+++ homepage/public/game/td/txt/ccConfig.js
@@ -0,0 +1,10 @@
+var ccConfig = {
+	"showFPS": true, 
+	"frameRate": 60, 
+	"project_type": "javascript", 
+	"debugMode": 1, 
+	"renderMode": 1, 
+	"id": "gameCanvas"
+};
+
+document["ccConfig"] = ccConfig;
