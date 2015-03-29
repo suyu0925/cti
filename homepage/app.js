78139a967f95bbea2a60487b9c7375a3150f025e
kivvi.yusu <kivvi.yusu@gmail.com>
2015-03-29 17:51:41 +0800

    update homepage.

--- homepage/app.js
+++ homepage/app.js
@@ -4,9 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var game = require('./routes/game');
-
+var router = express.Router();
 var app = express();
 
 // view engine setup
@@ -21,7 +19,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/game', game);
+/* GET home page. */
+router.get('/', function(req, res) {
+    console.log('redirect to index_1.htm');
+    res.redirect('/index_1.htm');
+});
+app.use(router);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
