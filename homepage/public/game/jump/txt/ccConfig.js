f30d31c2148baa3bf20dafa75a13c6a2af03a6c6
suyu0925 <suyu0925@gmail.com>
2015-05-04 16:41:45 +0800

    temp commit.

--- homepage/public/game/jump/txt/ccConfig.js
+++ homepage/public/game/jump/txt/ccConfig.js
@@ -1,10 +1,10 @@
 var ccConfig = {
-	"showFPS": false,
-	"frameRate": 60, 
-	"project_type": "javascript", 
-	"debugMode": 0, 
-	"renderMode": 1, 
-	"id": "gameCanvas"
+    "showFPS": false,
+    "frameRate": 60,
+    "project_type": "javascript",
+    "debugMode": 0,
+    "renderMode": 1, 
+    "id": "gameCanvas"
 };
 
 document["ccConfig"] = ccConfig;
