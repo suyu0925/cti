5f3a7d26101fdb79a94709ee66e91aa350177bd4
lckof-pc <suyu0925@gmail.com>
2015-05-16 02:26:29 +0800

    temp commit.

---
+++ homepage/public/game/miner/txt/ccConfig.js
@@ -0,0 +1,10 @@
+var ccConfig = {
+    "showFPS": false,
+    "frameRate": 60,
+    "project_type": "javascript",
+    "debugMode": 0,
+    "renderMode": 1, 
+    "id": "gameCanvas"
+};
+
+document["ccConfig"] = ccConfig;
